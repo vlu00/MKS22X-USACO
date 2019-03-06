07474561aa353adb3ab3c0a2256d2ded933b402e
vlu00 <vlu00@stuy.edu>
2019-03-06 14:49:48 -0500

    Working on translating the first row into ints

--- USACO.java
+++ USACO.java
@@ -1,34 +1,56 @@
+import java.util.*;
+import java.io.*;
+
 public class USACO{
-  public static int bronze(String filename){
+  public static int bronze(String filename) throws FileNotFoundException{
     File text = new File(filename);
     Scanner inf = new Scanner(text);
-    int[][] lake;
-    int i = 0;
-    int r = 0;
-    int c = 0;
-    int row, col, elevation, n;
+    //int[][] lake;
+    int i = 1;
+    int rows = 0;
+    int cols = 0;
+    int elevation = 0;
+    int n = 0;
 
-    while (inf.hasNext()) {
-      if (i == 0) {
-        row = Integer.parseInt(inf.next());
+    //while (inf.hasNextLine()) {
+      if (i == 1) {
+        File setupInfo = new File(inf.nextLine());
+        Scanner setup = new Scanner(setupInfo);
+        int index = 0;
+        while (setup.hasNext()) {
+          int num = Integer.parseInt(setup.next());
+          if (index == 0) {
+            rows = num;
+          }
+          if (index == 1) {
+            cols = num;
+          }
+          if (index == 2) {
+            elevation = num;
+          }
+          else {
+            n = num;
+          }
+          index++;
+        }
       }
-      else if  (i == 1) {
-        col = Integer.parseInt(inf.next());
-        lake = new int[row][col];
-      }
-      else if (i == 2) {
-        elevation = Integer.parseInt(inf.next());
-      }
-      else if (i == 3) {
-        n = Integer.parseInt(inf.next());
-      }
-      else if (i < row * col) {
-        
-      }
-      i++;
+    //}
+
+    System.out.println(rows);
+    System.out.println(cols);
+    System.out.println(elevation);
+    System.out.println(n);
+
+    return -10;
+  }
+
+  public static void main(String[] args) {
+    try {
+      System.out.println(bronze("makelake.in"));
     }
-    int[][] lake = new int[][];
-
+    catch (FileNotFoundException e) {
+      System.out.println("File Not Found");
+    }
   }
 
 }
